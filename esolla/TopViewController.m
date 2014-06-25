c468980c70016ba16d9aec98e02f067bf9d6feb6
tatsuaki_w <tatsuaki.w@gmail.com>
2014-06-25 21:51:52 +0900

    button add

--- esolla/TopViewController.m
+++ esolla/TopViewController.m
@@ -7,7 +7,6 @@
 //
 
 #import "TOPViewController.h"
-#import "CameraViewController.h"
 
 @interface TopViewController ()
 
@@ -30,14 +29,64 @@
 
     //タイトル追加
     UILabel *titleLabel = [[UILabel alloc]init];
-    titleLabel.text = @"touchでcamera start";
+    titleLabel.text = @"esolla app";
     titleLabel.numberOfLines = 2;
     titleLabel.font = [UIFont systemFontOfSize:40];
     titleLabel.frame = CGRectMake(0, 50, 320, 100);
     titleLabel.textColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:1];
     titleLabel.textAlignment = NSTextAlignmentCenter;
     [self.view addSubview:titleLabel];
+    
+    //start button create
+    UIImageView *startBtn = [[UIImageView alloc]init];
+    startBtn.backgroundColor = [UIColor colorWithRed:1.0 green:0.5 blue:0.2 alpha:1];
+    startBtn.frame = CGRectMake(110, 400, 100, 50);
+    [self.view addSubview:startBtn];
+    startBtn.clipsToBounds = YES;
+    startBtn.layer.cornerRadius = 20;
+    UILabel *btnLabel = [[UILabel alloc]init];
+    btnLabel.text = @"Camera";
+    btnLabel.font = [UIFont systemFontOfSize:20];
+    btnLabel.frame = CGRectMake(0, 0, 100, 50);
+    btnLabel.textColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:1];
+    btnLabel.textAlignment = NSTextAlignmentCenter;
+    [startBtn addSubview:btnLabel];
+    startBtn.userInteractionEnabled = YES;
+    startBtn.tag = 100;
+    UITapGestureRecognizer *startBtnGesture = [[UITapGestureRecognizer alloc]initWithTarget:
+                                               self action:@selector(tapAction:)];
+    [startBtn addGestureRecognizer:startBtnGesture];
+
+    
 }
+
+- (void)tapAction:(UIGestureRecognizer*)gesture
+{
+    //self.view.backgroundColor = [UIColor blackColor];
+    // 1.何をおしたか判別　=> tagを使う
+    if(gesture.view.tag == 100){
+        UIImagePickerController *pic;
+        pic = [[UIImagePickerController alloc]init];
+        pic.delegate = (id)self;
+        pic.sourceType = UIImagePickerControllerSourceTypeCamera;
+        pic.allowsEditing = TRUE;
+        
+        UIImageView *img = [[UIImageView alloc]init];
+        img.image = [UIImage imageNamed:@"01.png"];
+        img.frame = CGRectMake(100, 250, 120, 170);
+        
+        img.userInteractionEnabled = YES;
+        img.tag = 200;
+        
+        pic.cameraOverlayView = img;
+        [self presentViewController:pic animated:YES completion:nil];
+    } else {
+        //NSLog(gesture);
+        //img.frame = CGRectMake(100, 250, 120, 170);
+    }
+    return;
+}
+
 
 - (void)didReceiveMemoryWarning
 {
@@ -50,18 +99,7 @@
     /*
     CameraViewController *cameraViewCtr = [[CameraViewController alloc]init];
     [self presentViewController:cameraViewCtr animated:NO completion:nil];*/
-    UIImagePickerController *pic;
-    pic = [[UIImagePickerController alloc]init];
-    pic.delegate = (id)self;
-    pic.sourceType = UIImagePickerControllerSourceTypeCamera;
-    pic.allowsEditing = TRUE;
-    
-    UIImageView *logo = [[UIImageView alloc]init];
-    logo.image = [UIImage imageNamed:@"01.png"];
-    logo.frame = CGRectMake(100, 250, 120, 170);
 
-    pic.cameraOverlayView = logo;
-    [self presentViewController:pic animated:YES completion:nil];
     
     /*
      UIImagePickerController *picker = [[UIImagePickerController alloc] init];
