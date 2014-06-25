d3f4e67b401987315c1d1d62eaac3b46602e5c0d
tatsuaki_w <tatsuaki.w@gmail.com>
2014-06-25 20:07:16 +0900

    Initial Commit

---
+++ esolla/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  esolla
+//
+//  Created by tatsuaki watanabe on 2014/06/25.
+//  Copyright (c) 2014年 tatsuaki watanabe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ esollaTests/esollaTests.m
@@ -0,0 +1,34 @@
+//
+//  esollaTests.m
+//  esollaTests
+//
+//  Created by tatsuaki watanabe on 2014/06/25.
+//  Copyright (c) 2014年 tatsuaki watanabe. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface esollaTests : XCTestCase
+
+@end
+
+@implementation esollaTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
